3ba2652beb0b035c560471f14fcbd0d1da0d9774
bengorman <ben.gorman01@gmail.com>
2019-02-11 13:01:47 -0500

    added a procfile and changed port for heroku

--- server/config/app.js
+++ server/config/app.js
@@ -1,10 +1,14 @@
-var config = require('./config'), 
-    mongoose = require('mongoose'),   
+var config = require('./config'),
+    mongoose = require('mongoose'),
     express = require('./express');
 
 module.exports.start = function() {
   var app = express.init();
-  app.listen(config.port, function() {
-    console.log('App listening on port', config.port);
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = config.port;
+  }
+  app.listen(port, function() {
+    console.log('App listening on port', port);
   });
 };
