79c862d1d6a758bf96acfc2634d5afd0583003c4
bengorman <ben.gorman01@gmail.com>
2019-02-11 22:11:50 -0500

    changed port logic

--- server/config/config.js
+++ server/config/config.js
@@ -5,5 +5,5 @@ module.exports = {
   db: {
     uri: 'mongodb://bengorman:83mqwRMmmKDjrBU@ds147975.mlab.com:47975/cen3031',
   },
-  port: 8080
+  port: (process.env.PORT == null || process.env.PORT == "") ? 8080 : process.env.port,
 };
